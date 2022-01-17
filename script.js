2bd38a255be10b1b352777eb4c6226bdf09ff5aa
snigdhabommareddy <snigdha.bommareddy@borngroup.com>
2022-01-17 19:32:38 +0530

    done

---
+++ script.js
@@ -0,0 +1,23 @@
+(function () {
+    'use strict'
+    var forms = document.querySelectorAll('.needs-validation')
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
+
+function test(){
+    document.getElementById('coupon-content').style.visibility="visible"
+}
+
+function closeDiv(){
+    document.getElementById('coupon-content').style.visibility="hidden"
+}
